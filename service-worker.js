b8721ac6056cc7c14a3bc401727747262462aec1
summerstarlee <1326646676@qq.com>
2020-07-25 12:38:47 +0000

    Deploying to gh-pages from  @ 96e224d801f282f13782a23025eac92a2b30925d 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fcbbbbc22afb5166725f0a5f310bdf06.js"
+  "/github-actions-demo/precache-manifest.e3b406df08b1570ed01dbc3d747d8adc.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/github-actions-demo/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
